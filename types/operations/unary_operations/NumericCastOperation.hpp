169c1f37cfeb1d46d86af1f85cbc703215d22521
Hakan Memisoglu <hakanmemisoglu@apache.org>
2016-06-20 11:45:58 -0500

    Include DecimalType in NumericCastOperation.

--- types/operations/unary_operations/NumericCastOperation.hpp
+++ types/operations/unary_operations/NumericCastOperation.hpp
@@ -29,6 +29,7 @@
 #include "catalog/CatalogTypedefs.hpp"
 #include "storage/ValueAccessor.hpp"
 #include "storage/ValueAccessorUtil.hpp"
+#include "types/DecimalType.hpp"
 #include "types/DoubleType.hpp"
 #include "types/FloatType.hpp"
 #include "types/IntType.hpp"
